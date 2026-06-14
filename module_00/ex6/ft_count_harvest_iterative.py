a0bbeb722ed820796ce610e23403c434cb61a4b6
Alen Karakashyan <karakashyan2007@gmail.com>
2026-06-14 18:57:11 +0400

    module 00 done

--- module_00/ex6/ft_count_harvest_iterative.py
+++ module_00/ex6/ft_count_harvest_iterative.py
@@ -1,6 +1,7 @@
 def ft_count_harvest_iterative():
-	day = int(input("Days untill harvest: "))
-	i = 0
-	for i in range(i, 6):
+	day = int(input("Days until harvest: "))
+	i = 1
+	while i in range(i, day + 1):
 		print("Day", i)
+		i += 1
 	print("Harvest time!")
--- module_00/ex6/ft_count_harvest_recursive.py
+++ module_00/ex6/ft_count_harvest_recursive.py
@@ -1,11 +1,9 @@
+def helper_recursive(count):
+	if(1 < count):
+		helper_recursive(count - 1)
+	print("Day", count)
+
 def ft_count_harvest_recursive():
-	day = int(input("Days untill harvest: "))
-	start_day = 1
-	def helper_rec(current):
-		if current > day:
-			print("Harvest time!")
-			return
-		print("Day", current)
-		helper_rec(current + 1)
-	helper_rec(start_day)
-	
+	day = int(input("Days until harvest: "))
+	helper_recursive(day)
+	print("Harvest time!")
