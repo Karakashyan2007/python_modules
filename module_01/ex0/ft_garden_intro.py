7c53d416e5595dbb1e170dfcae84ee854da2c2ce
Alen Karakashyan <karakashyan2007@gmail.com>
2026-06-15 16:19:25 +0400

    finished M1E0

---
+++ module_01/ex0/ft_garden_intro.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+def main():
+	print("=== Welcome to My Garden ===")
+	plant = "Rose"
+	height = "55cm"
+	age = "80days"
+	print(f"Plant: {plant}")
+	print(f"Height: {height}")
+	print(f"Age: {age}")
+	print("=== End of Program ===")
+if __name__ == "__main__":
+	main()
